3121fdd7b1aa46d8b4e08f4e681b60a4de029556
SaifAlmajd <syfalmjd11@gmail.com>
2024-04-07 17:01:21 +0800

    Secuirty Improvements

--- lib/src/in_app_encryption.dart
+++ lib/src/in_app_encryption.dart
@@ -9,7 +9,8 @@ inAppEncrypt({required Object text, required Object key}) =>
 ///Cant be decrypted later
 ///
 /// It is used to store and compare string in database
-hashEncrypt({required Object text}) => _hashEncryption(text.toString());
+hashEncrypt({required Object text, required Object keyIV}) =>
+    _hashEncryption(text.toString(), keyIV.toString());
 
 String _inAppEncryption(plainText, ptKey) {
   if (ptKey.length > 32) {
@@ -25,10 +26,15 @@ String _inAppEncryption(plainText, ptKey) {
   return encrypted.base64;
 }
 
-String _hashEncryption(plainText) {
-  final keyBytes = Key.fromUtf8('wctdg4SXTIFvvGreJ91OQLkQRNqE99I6');
-  final iv = IV.fromLength(16);
-  final encrypter = Encrypter(Salsa20(keyBytes));
+///Hashing a text to compare with database
+String _hashEncryption(String plainText, String keyIV) {
+  final key = Key.fromUtf8('wctdg4SXTIFvvGreJ91OQLkQRNqE99I6');
+  final iv = IV.fromUtf8(keyIV); // Use a fixed IV
+  final encrypter = Encrypter(Salsa20(key));
+
+  // Encrypt the plaintext using the fixed IV
   final encrypted = encrypter.encrypt(plainText, iv: iv);
+
+  // Return the base64 representation of the encrypted text
   return encrypted.base64;
 }
